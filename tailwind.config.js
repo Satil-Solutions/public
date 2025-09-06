abd8501a92bbf88ab1a364115a5e5b475b08019f
Jonatan Bennerstål <frawd@live.se>
2025-09-06 17:44:37 +0200

    Merged Theme into project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    presets: [require('./themes/hugo-saasify-theme/tailwind.config.js')],
-    content: [
-      "./themes/hugo-saasify-theme/layouts/**/*.html",
-      "./layouts/**/*.html",
-      "./content/**/*.{html,md}"
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['Inter', 'system-ui', 'sans-serif'],
-          heading: ['Plus Jakarta Sans', 'sans-serif'],
+  content: [
+	"./layouts/**/*.html",
+    "./content/**/*.{html,md}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#eef1fc',
+          100: '#dde3f9',
+          200: '#bbc7f3',
+          300: '#99abec',
+          400: '#778fe6',
+          500: '#5573df',
+          600: '#425ad6',
+          700: '#3548ab',
+          800: '#283680',
+          900: '#1b2456',
         },
-        colors: {
-          green: {
+        secondary: {
+          50: '#faf5ff',
+          100: '#f3e8ff',
+          200: '#e9d5ff',
+          300: '#d8b4fe',
+          400: '#c084fc',
+          500: '#a855f7',
+          600: '#9333ea',
+          700: '#7e22ce',
+          800: '#6b21a8',
+          900: '#581c87',
+        },
+		 green: {
             100: '#E0FFE0',
           },
           blue: {
@@ -24,14 +42,19 @@ module.exports = {
           },
         },
       },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        heading: ['Plus Jakarta Sans', 'sans-serif'],
+      },
     },
-    plugins: [
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/typography'),
-    ],
-    safelist: [
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+  safelist: [
       'bg-red-100',
       'bg-green-100',
       'bg-blue-100',
     ],
-  }
+}
